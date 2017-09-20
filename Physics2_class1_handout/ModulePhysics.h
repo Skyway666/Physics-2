0a2022b5af2267bbdff1df78a358aa7567c70fb9
Skyway666 <lucasgarciamateu@gmail.com>
2017-09-20 10:35:47 +0200

    Phisics 2 first handout

---
+++ Physics2_class1_handout/ModulePhysics.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+#include "Box2D/Box2D/Box2D.h"
+#pragma comment (lib, "Box2D/libx86/Debug/Box2D.lib")
+
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+private:
+
+	bool debug;
+	b2World* hugo;
+};
